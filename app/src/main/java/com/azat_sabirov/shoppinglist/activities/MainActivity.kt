981b4650cc57ef96aaeb3fc4d434910e678dabc5
Azat_Sabirov <azat240510@gmail.com>
2021-10-21 19:01:50 +0300

    6-7. Создание BottomNavView , FragmentManager

--- app/src/main/java/com/azat_sabirov/shoppinglist/activities/MainActivity.kt
+++ app/src/main/java/com/azat_sabirov/shoppinglist/activities/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.azat_sabirov.shoppinglist.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.azat_sabirov.shoppinglist.R
+import com.azat_sabirov.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomNavListener()
+    }
+
+    fun setBottomNavListener() {
+        binding.bottomNavMenu.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Log.d("MyLog", "settings")
+                }
+                R.id.notes -> {
+                    Log.d("MyLog", "notes")
+                }
+                R.id.shop_list -> {
+                    Log.d("MyLog", "shop_list")
+
+                }
+                R.id.new_item -> {
+                    Log.d("MyLog", "new_item")
+                }
+            }
+            true
+        }
     }
 }
---
+++ app/src/main/java/com/azat_sabirov/shoppinglist/fragments/FragmentManager.kt
@@ -0,0 +1,15 @@
+package com.azat_sabirov.shoppinglist.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import com.azat_sabirov.shoppinglist.R
+
+object FragmentManager {
+    var currentFrag: BaseFragment? = null
+
+    fun setFragment(newFrag: BaseFragment, activity: AppCompatActivity){
+        val transaction = activity.supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.place_holder, newFrag)
+        transaction.commit()
+        currentFrag = newFrag
+    }
+}
