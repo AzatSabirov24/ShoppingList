83ba9b8a4bd473170c3df575957d3f7619386dd3
Azat_Sabirov <azat240510@gmail.com>
2021-11-16 21:39:43 +0300

    27. Добавлен ShopListNameAdapter.kt

--- app/src/main/java/com/azat_sabirov/shoppinglist/fragments/ShopListNamesFragment.kt
+++ app/src/main/java/com/azat_sabirov/shoppinglist/fragments/ShopListNamesFragment.kt
@@ -8,15 +8,18 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.azat_sabirov.shoppinglist.activities.MainApp
 import com.azat_sabirov.shoppinglist.databinding.FragmentShopListNamesBinding
 import com.azat_sabirov.shoppinglist.db.MainViewModel
+import com.azat_sabirov.shoppinglist.db.ShopListNameAdapter
 import com.azat_sabirov.shoppinglist.dialogs.NewListDialog
 import com.azat_sabirov.shoppinglist.entities.ShoppingListName
 import com.azat_sabirov.shoppinglist.utils.TimeManager
 
 class ShopListNamesFragment : BaseFragment() {
     private lateinit var binding: FragmentShopListNamesBinding
+    private val adapter = ShopListNameAdapter()
 
     private val mainViewModel: MainViewModel by activityViewModels {
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
@@ -41,6 +44,7 @@ class ShopListNamesFragment : BaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -54,15 +58,17 @@ class ShopListNamesFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRcView()
+        observer()
     }
 
     private fun initRcView() = with(binding) {
-
+        rcView.layoutManager = LinearLayoutManager(activity)
+        rcView.adapter = adapter
     }
 
-    private fun observe(){
+    private fun observer(){
         mainViewModel.allShoppingListNames.observe(viewLifecycleOwner, {
-
+            adapter.submitList(it)
         })
     }
 
